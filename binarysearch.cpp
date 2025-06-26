24561e733b1bcbd08fdfbb125b5b702c25b019fc
Rouf1224 <ahmadroufr24@gmail.com>
2025-06-26 18:23:41 +0700

    menambahkan step step

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -30,6 +30,16 @@ class BinaryTree
         int x;
         cout << "Masukkan nilai : ";
         cin >> x;
+
+        // Step 1: Allocate memory for the new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data field of new node
+        newNode->info =x;
+        
+        // Step 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
     }
 }
 
