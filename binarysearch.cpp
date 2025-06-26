5845dd386951ecd1955e73ea1e42f96454dde3e7
Rouf1224 <ahmadroufr24@gmail.com>
2025-06-26 18:59:01 +0700

    menambahkan data pada void preorder dan postorder

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -114,7 +114,33 @@ class BinaryTree
     void preorder(Node *ptr)
     {
         if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+        return;
+
+        cout << ptr->info << " "; //parent
+        preorder(ptr->leftchild);
+        preorder(ptr->rightchild);
     }
+
+     void postorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+        return;
+
+        postorder(ptr->leftchild);
+        postorder(ptr->rightchild);
+        cout << ptr->info << " "; //parent
+    }
+
 
 };
 
