0da0189ed280cc7b9acda010672300ace1ae1ac6
Rouf1224 <ahmadroufr24@gmail.com>
2025-06-26 18:39:00 +0700

    menambahkan void search

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -67,8 +67,17 @@ class BinaryTree
         // Step 7: If the value in the data field of the new node is greater than that of the parent
         else if (x > parent->info)
         {
-            
+            // 7a : make the right child of parent point to the new node
+            parent->rightchild = newNode;
+
+            // 7b : Exit
+            return;
         }
+    }
+
+    void search(int element, Node *&parent, Node *&currentNode)
+    {
+        
     }
 };
 
